79d3d2a73e4cb269e633b007adaf77dce9cf3626
Yug Panchal <panchalyug82@gmail.com>
2026-06-30 22:05:18 +0530

    api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 
-import axios from "axios"; 
-const API = axios.create({ 
-  baseURL: "http://127.0.0.1:8000", 
-}); 
-export default API;
+import axios from "axios";
 
+const API = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+});
+
+export default API;
